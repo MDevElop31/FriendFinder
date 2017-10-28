9989bb1ab2f6fd8b089f2990e664f0e7b6011daf
Michael Devaney <Mike@Mike.local>
2017-10-27 21:29:25 -0400

    added and fixed files

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,19 @@
+const friends = require("../data/friends.js");
+
+module.exports = function(app){
+
+	app.get('/api/friends', function (req, res) {
+
+		friends.getCurrentList()
+			.then( (allFriends)=> res.json(allFriends) )
+			.catch( (err)=> {if (err) console.log(err)} );
+	});
+
+	app.post('/api/friends', function (req, res){
+
+		friends.matchToFriend(req.body).then((friend)=>{
+			res.json(friend);
+		}).catch((err)=>{if (err) console.log(err);});
+
+	});
+};
